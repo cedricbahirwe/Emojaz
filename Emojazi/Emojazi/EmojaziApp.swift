43c374e3513e16e8de47dd69f410a795af138bee
Cédric Bahirwe <cedricaganzebahirwe@gmail.com>
2022-01-12 14:44:35 +0200

    Add Structure and basic data

---
+++ Emojazi/Emojazi/EmojaziApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojaziApp.swift
+//  Emojazi
+//
+//  Created by Cédric Bahirwe on 12/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct EmojaziApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Emojazi/Emojazi/Models/Emoji.swift
@@ -0,0 +1,32 @@
+//
+//  Emoji.swift
+//  Emojazi
+//
+//  Created by Cédric Bahirwe on 12/01/2022.
+//
+
+import Foundation
+
+// MARK: - Emoji
+struct Emoji: Codable, Identifiable {
+    let codes, char, name, category: String
+    let group: EmojiGroup
+    let subgroup: String
+    
+    var id: String { codes + name }
+}
+
+enum EmojiGroup: String, Codable {
+    case activities = "Activities"
+    case animalsNature = "Animals & Nature"
+    case component = "Component"
+    case flags = "Flags"
+    case foodDrink = "Food & Drink"
+    case objects = "Objects"
+    case peopleBody = "People & Body"
+    case smileysEmotion = "Smileys & Emotion"
+    case symbols = "Symbols"
+    case travelPlaces = "Travel & Places"
+}
+
+typealias Emojis = [Emoji]
